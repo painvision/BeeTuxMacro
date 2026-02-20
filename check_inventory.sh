a94df096ad84309eecce972c61cbbd994ca478b0
painvision <pabcihba@gmail.com>
2026-02-20 09:44:37 +0300

    Update check_inventory.sh

--- check_inventory.sh
+++ check_inventory.sh
@@ -6,13 +6,12 @@
 START_TIME=$SECONDS
 while :
 do
-    ELAPSED=$(( SECONDS - START_TIME ))
-    if pixel_in_red_range $(get_pixel_coords); then
-        if [ "$ELAPSED" -gt "$FARM_SECONDS" ]; then
-            pkill -f farm.sh
-            bash -c ~/BeeTuxMacro/after_farm.sh
-            pkill -f check_inventory.sh
-        fi
-    fi
-    sleep 1
+ELAPSED=$(( SECONDS - START_TIME ))
+if pixel_in_red_range $(get_pixel_coords) || [ "$ELAPSED" -gt "$FARM_SECONDS" ]; then
+pkill -f farm.sh
+bash -c ~/BeeTuxMacro/after_farm.sh
+pkill -f check_inventory.sh
+fi
+fi
+sleep 1
 done
